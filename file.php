60d41ebd12c0255ca29d88a6e29edcc0fad0f427
Nishika Soni <nishika@webexpertinfotech.com>
2026-09-02 14:49:25 +0530

    Json encode & decode

--- file.php
+++ file.php
@@ -1,56 +1,56 @@
 <?php
 
 // 1. create a new text file named hello.txt.
-// $file=fopen("hello.txt", "w");
+$file=fopen("hello.txt", "w");
 
-// if($file){
-//     echo "File Created Successfully!!";
-//     fclose($file);
-// }
-// else{
-//     echo "Failed to open file";
-// }
+if($file){
+    echo "File Created Successfully!!";
+    fclose($file);
+}
+else{
+    echo "Failed to open file";
+}
 
 
 
 // 2. Create a file named students.txt and write "Welcome to PHP File Handling" into it.
-// $file2=fopen("students.txt", "w");
+$file2=fopen("students.txt", "w");
 
-// if($file2){
-//     $text="Welcome to PHP File Handling";
-//     fwrite($file2, $text);
-//     fclose($file2);
-// }
-// else{
-//     echo "File Not Created";
-// }
+if($file2){
+    $text="Welcome to PHP File Handling";
+    fwrite($file2, $text);
+    fclose($file2);
+}
+else{
+    echo "File Not Created";
+}
 
 
 
 // 3. Open an existing file data.txt in read mode and display its contents.
-// $file3=fopen("data.txt", "r");
+$file3=fopen("data.txt", "r");
 
-// if($file3){
-//     echo "File Opened Successfully!!" . "<br><br>";
-//     // echo readfile("data.txt");                           
-//     echo fread($file3, filesize("data.txt"));              
-//     fclose($file3);
-// }
-// else{
-//     echo "File Not Exist";
-// }
+if($file3){
+    echo "File Opened Successfully!!" . "<br><br>";
+    // echo readfile("data.txt");                           
+    echo fread($file3, filesize("data.txt"));              
+    fclose($file3);
+}
+else{
+    echo "File Not Exist";
+}
 
 
 
 // 4. Write a PHP program to check whether data.txt exists or not before opening it.
-// $file4=file_exists("data.txt");
+$file4=file_exists("data.txt");
 
-// if($file4){
-//     echo "File Exists!!";
-// }
-// else{
-//     echo "File Not Exists";
-// }
+if($file4){
+    echo "File Exists!!";
+}
+else{
+    echo "File Not Exists";
+}
 
 
 
---
+++ json.php
@@ -0,0 +1,14 @@
+<!-- encode -->
+<!-- <?php
+$age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
+
+echo json_encode($age);
+?> -->
+
+
+<!-- decode -->
+<!-- <?php
+$jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
+
+var_dump(json_decode($jsonobj, true));
+?> -->
