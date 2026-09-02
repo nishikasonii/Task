b105025bb319bf07c3610bb45277e78f41cd8fa5
Nishika Soni <nishika@webexpertinfotech.com>
2026-09-02 14:52:22 +0530

    json encode & decode

--- json.php
+++ json.php
@@ -1,14 +1,14 @@
 <!-- encode -->
-<!-- <?php
+<?php
 $age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
 
 echo json_encode($age);
-?> -->
+?>
 
 
 <!-- decode -->
-<!-- <?php
+<?php
 $jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
 
 var_dump(json_decode($jsonobj, true));
-?> -->
+?>
